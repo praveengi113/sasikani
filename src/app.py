84a6b27a5d99acf8ff3cc8aca82abc99e6a0ea99
praveengi113 <praveenkumar.p113@gmail.com>
2020-10-17 11:19:43 +0530

    make it better

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, current_app
+from src.common.post import Feed
+
+app = Flask(__name__)
+"""
+@app.route('/')
+def index():
+    covid_data_log = Feed.from_mongo_all()
+    covid_data = Feed.from_mongo()
+    cases = covid_data[0]["cases"]
+    cured = covid_data[0]["cured"]
+    death = covid_data[0]["death"]
+    date = covid_data[0]["time"]
+    return render_template('cards_try.html', cases=cases, cured=cured, death=death, date=date, log=covid_data_log)
+"""
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
