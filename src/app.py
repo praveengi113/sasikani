52185780b3e0e1ee9584f47be7ab7ab3f0d37482
praveengi113 <praveenkumar.p113@gmail.com>
2020-10-19 14:20:31 +0530

    mail debug mode on

--- src/app.py
+++ src/app.py
@@ -124,14 +124,14 @@ def submit_order():
 
 		dict_new = {'name':customer_name,'address':customer_address,"phone":customer_phone,"mail":recpient_email_1,"order_time":time_now,"ref_no":ref_no,"payment_option":payment_option}
 
-		msg.html = render_template('invoice.html',invoice_table=invoice_table,total_amt=total_amt,invoice_details=dict_new,payment_option=payment_option)
+		#msg.html = render_template('invoice.html',invoice_table=invoice_table,total_amt=total_amt,invoice_details=dict_new,payment_option=payment_option)
 		#requests.post("https://api.mailgun.net/v3/sandbox9ac083f02e414f50a0541191e04fe126.mailgun.org", auth = ("api","c4a2d57211829e33d0d81cfbd8f8f881-53c13666-64770e48"), data = {"from": "<app187924775@heroku.com>", "to": ["crackerssasikani@gmail.com"], "subject": "Order Placed", "html": "<html>Hello There</html>"})
 		msg = MIMEText('Testing some Mailgun awesomness')
 		msg['Subject'] = "Hello"
-		msg['From'] = "app187924775@heroku.com"
+		msg['From'] = "praveen113kumar@hotmail.com"
 		msg['To'] = "crackerssasikani@gmail.com"
-		s = smtplib.SMTP('smtp.mailgun.org', 587)
-		s.login('postmaster@sandbox9ac083f02e414f50a0541191e04fe126.mailgun.org','4bebc3e0bc1a2f39d1c82dadddfef01b-53c13666-ca1ee0e3')
+		s = smtplib.SMTP('smtp.live.com', 587)
+		s.login('praveen113kumar@hotmail.com','revathi@113')
 		s.sendmail(msg['From'],msg['To'],msg.as_string())
 		s.quit()
 
